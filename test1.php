088e1a4da3277ff5e4cb6c27bccd0ddbb3a659b2
Laddima <38899915+Laddima@users.noreply.github.com>
2018-08-11 14:34:00 -0400

    Add files via upload

---
+++ test1.php
@@ -0,0 +1,239 @@
+
+<!doctype html>
+<html lang="en"><head>
+
+
+	  
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+       <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+	<div class="container">
+	 <div class="col-12">
+
+				 
+		 
+		 
+		 <div class="row">
+  <div class="col-4"><h1>Lorem Ipsum</h1>
+		<img src="http://cdn.dota2.com/apps/dota2/images/heroes/axe_lg.png" >
+		<br><br>
+			 	<p>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+			 
+			 </div>
+  <div class="col-8">
+		 <h3>Starting</h3>
+		 
+		 <div class="row">
+    <div class="col-sm">
+     <img src="http://cdn.dota2.com/apps/dota2/images/items/sphere_lg.png" >
+    </div>
+    <div class="col-sm">
+      <img src="http://cdn.dota2.com/apps/dota2/images/items/sphere_lg.png" >
+    </div>
+    <div class="col-sm">
+      <img src="http://cdn.dota2.com/apps/dota2/images/items/sphere_lg.png" >
+    </div>
+  	
+    <div class="col-sm">
+     <img src="http://cdn.dota2.com/apps/dota2/images/items/sphere_lg.png" >
+    </div>
+    <div class="col-sm">
+      <img src="http://cdn.dota2.com/apps/dota2/images/items/sphere_lg.png" >
+    </div>
+    <div class="col-sm">
+      <img src="http://cdn.dota2.com/apps/dota2/images/items/sphere_lg.png" >
+    </div>
+  	</div>
+		<br>
+		  <h3>Core</h3>
+		  <div class="row">
+    <div class="col-sm">
+     <img src="http://cdn.dota2.com/apps/dota2/images/items/sphere_lg.png" >
+    </div>
+    <div class="col-sm">
+      <img src="http://cdn.dota2.com/apps/dota2/images/items/sphere_lg.png" >
+    </div>
+    <div class="col-sm">
+      <img src="http://cdn.dota2.com/apps/dota2/images/items/sphere_lg.png" >
+    </div>
+  	
+    <div class="col-sm">
+     <img src="http://cdn.dota2.com/apps/dota2/images/items/sphere_lg.png" >
+    </div>
+    <div class="col-sm">
+      <img src="http://cdn.dota2.com/apps/dota2/images/items/sphere_lg.png" >
+    </div>
+    <div class="col-sm">
+      <img src="http://cdn.dota2.com/apps/dota2/images/items/sphere_lg.png" >
+    </div>
+  	</div>
+		 <br>  <h3>Late</h3>
+		
+		  <div class="row">
+    <div class="col-sm">
+     <img src="http://cdn.dota2.com/apps/dota2/images/items/sphere_lg.png" >
+    </div>
+    <div class="col-sm">
+      <img src="http://cdn.dota2.com/apps/dota2/images/items/sphere_lg.png" >
+    </div>
+    <div class="col-sm">
+      <img src="http://cdn.dota2.com/apps/dota2/images/items/sphere_lg.png" >
+    </div>
+  	
+    <div class="col-sm">
+     <img src="http://cdn.dota2.com/apps/dota2/images/items/sphere_lg.png" >
+    </div>
+    <div class="col-sm">
+      <img src="http://cdn.dota2.com/apps/dota2/images/items/sphere_lg.png" >
+    </div>
+    <div class="col-sm">
+      <img src="http://cdn.dota2.com/apps/dota2/images/items/sphere_lg.png" >
+    </div>
+  	</div>
+		 <br>  <h3>Sitch</h3>
+		
+		   <div class="row">
+    <div class="col-sm">
+     <img src="http://cdn.dota2.com/apps/dota2/images/items/sphere_lg.png" >
+    </div>
+    <div class="col-sm">
+      <img src="http://cdn.dota2.com/apps/dota2/images/items/sphere_lg.png" >
+    </div>
+    <div class="col-sm">
+      <img src="http://cdn.dota2.com/apps/dota2/images/items/sphere_lg.png" >
+    </div>
+  	
+    <div class="col-sm">
+     <img src="http://cdn.dota2.com/apps/dota2/images/items/sphere_lg.png" >
+    </div>
+    <div class="col-sm">
+      <img src="http://cdn.dota2.com/apps/dota2/images/items/sphere_lg.png" >
+    </div>
+    <div class="col-sm">
+      <img src="http://cdn.dota2.com/apps/dota2/images/items/sphere_lg.png" >
+    </div>
+  	</div></div>
+</div>
+		
+			  
+		 <br>
+	
+	
+<div class="container">
+  <div class="post-comments">
+
+    <form>
+      <div class="form-group">
+        <label for="comment">Your Comment</label>
+        <input name="comment" class="form-control" type="text"></textarea>
+      </div>
+    </form>
+
+  
+
+    <div class="row">
+
+      
+      <!-- first comment -->
+     
+            <!-- answer to the first comment -->
+
+          </div>
+	  
+	  
+    <div class="media text-muted pt-3">
+        
+          <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+            <strong class="text-gray-dark badge badge-secondary" style="font-size: 14px;">@username</strong>
+			  
+			  <i class="far fa-thumbs-up fa-pull-right" style="line-height: 18px;"></i>
+			<i class="fas fa-thumbs-down fa-pull-right" style="line-height: 18px;"></i>
+  			<span class="d-block text-gray-dark fa-pull-right">+3</span>
+		
+			<br>
+            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+          </p>
+
+
+        </div>  
+	
+	  <div class="media text-muted pt-3">
+        
+          <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+            <strong class="text-gray-dark badge badge-secondary" style="font-size: 14px;">@username</strong>
+			  
+			  <i class="far fa-thumbs-up fa-pull-right" style="line-height: 18px;"></i>
+			<i class="fas fa-thumbs-down fa-pull-right" style="line-height: 18px;"></i>
+  			<span class="d-block text-gray-dark fa-pull-right">+3</span>
+		
+			<br>
+            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+          </p>
+
+
+        </div> 
+	
+	  <div class="media text-muted pt-3">
+        
+          <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+            <strong class="text-gray-dark badge badge-secondary" style="font-size: 14px;">@username</strong>
+			  
+			  <i class="far fa-thumbs-up fa-pull-right" style="line-height: 18px;"></i>
+			<i class="fas fa-thumbs-down fa-pull-right" style="line-height: 18px;"></i>
+  			<span class="d-block text-gray-dark fa-pull-right">+3</span>
+		
+			<br>
+            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+          </p>
+
+
+        </div> 
+	
+	  <div class="media text-muted pt-3">
+        
+          <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+            <strong class="text-gray-dark badge badge-secondary" style="font-size: 14px;">@username</strong>
+			  
+			  <i class="far fa-thumbs-up fa-pull-right" style="line-height: 18px;"></i>
+			<i class="fas fa-thumbs-down fa-pull-right" style="line-height: 18px;"></i>
+  			<span class="d-block text-gray-dark fa-pull-right">+3</span>
+		
+			<br>
+            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+          </p>
+
+
+        </div> 
+	
+	
+	</div>
+        <!-- comments -->
+	
+	
+	</div>
+
+      </div><!-- Container -->
+   
+ </div><!-- Container -->
+   
+
+
+
+     <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  <div style='text-align: right;position: fixed;z-index:9999999;bottom: 0; width: 100%;cursor: pointer;line-height: 0;display:block !important;'><a title="Hosted on free web hosting 000webhost.com. Host your own website for FREE." target="_blank" href="https://www.000webhost.com/?utm_source=000webhostapp&amp;utm_campaign=000_logo&amp;utm_medium=website_dotabuilds&amp;utm_content=footer_img"><img src="https://cdn.rawgit.com/000webhost/logo/e9bd13f7/footer-powered-by-000webhost-white2.png"  alt="www.000webhost.com"></a></div></body>
+</html>
+
